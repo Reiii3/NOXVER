6e5b0bec94331c649a121fb3d28e604a847a0a64
Reiii3 <mohtoyib79@gmail.com>
2024-12-06 18:21:27 +0700

    update remove.sh

--- remove.sh
+++ remove.sh
@@ -9,25 +9,25 @@ echo
 sleep 1
 echo "───────────────────────────────────────────────────────"""
 #// Unistaler By Reii
-    setprop debug.sf.hw 0
-		setprop debug.egl.hw 0
-		setprop debug.egl.sync 1
-		setprop debug.gpuprio 5
-    setprop debug.cpuprio 5
-    setprop debug.ioprio 6
-    setprop debug.performance.tuning 0
-    setprop debug.performance.profile 0
-    setprop debug.performance.force false
-    setprop debug.performance_schema 0
-    setprop debug.power.profile balance
-    setprop debug.multicore.processing 0
-		performance=false
-		setprop debug.composition.type cpu
-    setprop debug.hwui.renderer opengl
-    cmd power set-fixed-performance-mode-enabled false
-    cmd power set-adaptive-power-saver-enabled true
-    cmd power set-mode 1
-    setprop debug.game.mode close
+            setprop debug.sf.hw 0
+            setprop debug.egl.hw 0
+            setprop debug.egl.sync 1
+            setprop debug.cpuprio 5
+            setprop debug.gpuprio 5
+            setprop debug.ioprio 5
+            setprop debug.performance.tuning 0
+            setprop debug.performance.profile 0
+            setprop debug.performance.force false
+            setprop debug.power.profile medium
+            setprop debug.performance_schema 0
+            setprop debug.multicore.processing 0
+            setprop debug.systemuicompilerfilter balanced
+            performance=true
+            setprop debug.composition.type cpu
+            setprop debug.hwui.renderer opengl
+            cmd power set-fixed-performance-mode-enabled false
+            cmd power set-adaptive-power-saver-enabled true
+            cmd power set-mode 1
   echo "[${runPackage}] Removing Succesfuly"
   echo "Next Remove Redionpl If installed"
 echo "──────────────────────────────────────────────────────"""
