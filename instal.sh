4925ebeaa35496357990c7f08321d2fa6dd5c012
Reiii3 <mohtoyib79@gmail.com>
2025-01-03 19:10:06 +0700

    update instal.sh

--- instal.sh
+++ instal.sh
@@ -178,6 +178,7 @@ case $1 in
             setprop debug.performance.tuning 1
             setprop debug.performance.profile 1
             setprop debug.performance.force true
+            setprop debug.hwui.disable_vsync true
             setprop debug.power.profile high_performance
             setprop debug.performance_schema 1
             setprop debug.multicore.processing 1
