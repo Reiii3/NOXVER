bbb9c6b143e9276f5a952ad9ee7d0bf106c622f8
Reiii3 <mohtoyib79@gmail.com>
2025-04-14 20:59:57 +0700

    update instal.sh

--- instal.sh
+++ instal.sh
@@ -36,6 +36,18 @@ echo
 sleep 1
 
 
+
+
+
+
+
+
+
+
+
+
+
+
 usefl=false
 	
 if [ -n "$1" ] && [ "$1" == "-p" ];then
@@ -61,7 +73,6 @@ else
     sleep 0.5
     echo "Add Game         : [${runPackage}]"
     sleep 0.5
-    settings delete global updatable_driver_production_opt_in > /dev/null 2>&1
    fi
 fi
 
@@ -147,118 +158,29 @@ else
   echo 
 
 case $1 in
-       "--collingmod")
-           setprop debug.sf.hw 0
-            setprop debug.egl.hw 0
-            setprop debug.egl.sync 1
-            setprop debug.cpuprio 5
-            setprop debug.gpuprio 5
-            setprop debug.ioprio 5
-            setprop debug.performance.tuning 0
-            setprop debug.performance.profile 0
-            setprop debug.performance.force false
-            setprop debug.power.profile medium
-            setprop debug.performance_schema 0
-            setprop debug.multicore.processing 0
-            setprop debug.systemuicompilerfilter balanced
-            performance=true
-            setprop debug.composition.type cpu
+       "--collingmod" )
 	          echo "┌[ Actived Colling System ]"
 	          sleep 0.5
             ;;
        "--performance" | *)
-            setprop debug.sf.hw 1
-            setprop debug.egl.hw 1
-            setprop debug.egl.sync 0
-            setprop debug.cpuprio 6
-            setprop debug.gpuprio 7
-            setprop debug.ioprio 7
-            setprop debug.performance.tuning 1
-            setprop debug.performance.profile 1
-            setprop debug.performance.force true
-            setprop debug.hwui.disable_vsync true
-            setprop debug.power.profile high_performance
-            setprop debug.performance_schema 1
-            setprop debug.multicore.processing 1
-            setprop debug.systemuicompilerfilter speed
-            performance=true
-            setprop debug.composition.type gpu
             echo "┌>[ Actived DionX Performance ]"
             sleep 0.5
             ;;
 esac
 
-if [ "${runPackage}" = "com.mojang.minecraftpe" ]; then
-   setprop debug.egl.force_fxaa false
-   setprop debug.egl.force_taa false
-   setprop debug.egl.force_msaa false
-   setprop debug.egl.force_ssaa false
-   setprop debug.egl.force_smaa false
-   setprop debug.egl.force_mlaa false
-   setprop debug.egl.force_txaa false
-   setprop debug.egl.force_csaa false
-     echo "├>[ Actived Minecraft Tuning ]"
-     sleep 0.5
-elif [ "${runPackage}" = "net.atlasclient.atlaspe" ]; then
-   setprop debug.egl.force_fxaa false
-   setprop debug.egl.force_taa false
-   setprop debug.egl.force_msaa false
-   setprop debug.egl.force_ssaa false
-   setprop debug.egl.force_smaa false
-   setprop debug.egl.force_mlaa false
-   setprop debug.egl.force_txaa false
-   setprop debug.egl.force_csaa false
-     echo "├>[ Actived Atlas Client Tuning ]"
-     sleep 0.5
-else 
-   setprop debug.egl.force_fxaa true
-   setprop debug.egl.force_taa true
-   setprop debug.egl.force_msaa true
-   setprop debug.egl.force_ssaa true
-   setprop debug.egl.force_smaa true
-   setprop debug.egl.force_mlaa true
-   setprop debug.egl.force_txaa true
-   setprop debug.egl.force_csaa true
-fi
-
 #// cmd By Reii
- setprop debug.hwui.renderer vulkan
- setprop debug.renderengine.backend skiavkthreaded
- setprop debug.hwui.render_performance true
- cmd thermalservice override-status 0
- cmd power set-fixed-performance-mode-enabled true
- cmd power set-adaptive-power-saver-enabled false
- cmd package compile -m speed --secondary-dex -f ${runPackage} > /dev/null 2>&1
- cmd power set-mode 0
+ 
  echo "├>[ Compiler System && Renderer Succes ]"
  sleep 0.5
  
  #// Driver Game By Mang Levv
- settings put global updatable_driver_production_opt_in_apps ${runPackage}
+
  echo "├>[ Actived Driver Game ]"
  sleep 0.5
  
- if [ $(getprop ro.hardware.egl) == meow ]; then
-       setprop debug.mtk.chiptuning.enable 1
-       setprop debug.mtk.gpu.mode 1
-       echo "├>[ Actived Mediatek Tuner ]"
-       sleep 0.5
- else 
-     echo "├>[$(getprop ro.product.brand)] not supported"
-     sleep 0.5
-fi
 
 # Render Optimization
-setprop debug.hwui.disable_draw_defer true
-setprop debug.hwui.disable_draw_reorder true
-setprop debug.hwui.render_dirty_regions false
-setprop debug.hwui.skip_empty_damage true
-setprop debug.hwui.disable_vsync true
-setprop debug.hwui.fps_divisor 1
-setprop debug.hwui.render_thread true
-setprop debug.hwui.render_thread_count 1
-setprop debug.hwui.use_gpu_pixel_buffers false
-setprop debug.hwui.use_buffer_age false
+
 
 echo "[├>[ Gpu && Rendering Optimization Actived ]"
 
