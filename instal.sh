2d9a53b532a3d3e360b789bd2a9f5247517acdc3
Reiii3 <mohtoyib79@gmail.com>
2025-04-15 21:17:28 +0700

    update instal.sh

--- instal.sh
+++ instal.sh
@@ -1,6 +1,7 @@
 $AXFUN
 bin="/data/local/tmp"
 local url_detect="https://reiii3.github.io/Center-Module/update/nox-update.sh"
+local url_core="https://reiii3.github.io/NOXVER/"
 import axeron.prop
 local core="r17rYI0tYD6Cp9pPOtlQ2c0rYMzuOEctdEmseIcseHlP29kC2QyrYAcvaZ1Ez9DPOyctd9lC21yrN4mt2ycsXnmP29pQJ5qrR=="
 update="$bin/detecUpdate"
