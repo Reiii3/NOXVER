98c82fb305d997cfb0a7bbfc31effa05e5a5d0f8
Reiii3 <mohtoyib79@gmail.com>
2025-04-15 21:59:56 +0700

    update remove.sh

--- remove.sh
+++ remove.sh
@@ -37,26 +37,7 @@ echo "Game Removed : ${runPackage}"
 sleep 1
 echo "───────────────────────────────────────────────────────"
 #// Unistaler By Reii
-            setprop debug.sf.hw 0
-            setprop debug.egl.hw 0
-            setprop debug.egl.sync 1
-            setprop debug.cpuprio 5
-            setprop debug.gpuprio 5
-            setprop debug.ioprio 5
-            setprop debug.performance.tuning 0
-            setprop debug.performance.profile 0
-            setprop debug.performance.force false
-            setprop debug.hwui.disable_vsync true
-            setprop debug.power.profile medium
-            setprop debug.performance_schema 0
-            setprop debug.multicore.processing 0
-            setprop debug.systemuicompilerfilter balanced
-            performance=false
-            setprop debug.composition.type cpu
-            setprop debug.hwui.renderer opengl
-            cmd power set-fixed-performance-mode-enabled false > /dev/null 2>&1
-            cmd power set-adaptive-power-saver-enabled true > /dev/null 2>&1
-            cmd power set-mode 1 > /dev/null 2>&1
+  
   echo "[${runPackage}] Removing Succesfuly"
   sleep 0.5
   echo "Next Remove Redionpl If installed"
