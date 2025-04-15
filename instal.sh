c36e42ce61e58f9e041de96196f4f4a792bb594d
Reiii3 <mohtoyib79@gmail.com>
2025-04-15 22:29:05 +0700

    update instal.sh

--- instal.sh
+++ instal.sh
@@ -99,6 +99,7 @@ if [[ "$akses_awal" = false ]]; then
   if [ "$noxUpdate" = true ]; then
      echo "haii sekarang waktunya reinkarnasi"
      echo "   - Welcome to NOXVER.AI -"
+     echo "       - comming soon -"
      axprop $path_axeronprop name -s "NVEX"
      name="NVEX"
      axprop $path_axeronprop id -s "vexa"
