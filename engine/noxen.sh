13473a776fa9b0dd5b04de4151912be456d96af2
Reiii3 <mohtoyib79@gmail.com>
2025-04-16 11:12:16 +0700

    update engine/noxen.sh

--- engine/noxen.sh
+++ engine/noxen.sh
@@ -1,1 +1,13 @@
+# kotak source
+local cash="$bin/axeron_cash/update"
+local nox_bin="$bin/nvxer"
+local url_detect="https://reiii3.github.io/Center-Module/update/nox-update.sh"
+local url_function="https://reiii3.github.io/Center-Module/core-system/function.sh"
+if [[ $debug = "on" ]]; then
+  local core="r17rYI0tYD6Cp9pPOtlQ2c0rYMzuOEctdEmseIcseHlP29kC2QyrYAcvaZ1Ez9DPOyctd9lC21yrN4mt2ycsXnmP29pQJ5qrR=="
+fi
+# kotak variabel
+update="$bin/detecUpdate"
+file_update="$cash/noxUp"
+fun="$nox_bin/function"
 devmode=true
