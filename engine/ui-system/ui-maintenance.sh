686b48415407a6da6724753cc00cf59a121b7ea9
Reiii3 <mohtoyib79@gmail.com>
2025-04-16 11:12:12 +0700

    update engine/ui-system/ui-maintenance.sh

--- engine/ui-system/ui-maintenance.sh
+++ engine/ui-system/ui-maintenance.sh
@@ -1,1 +1,10 @@
-
+data="/data/local/tmp/nxver"
+engine="$data/engine"
+. $engine
+echo "haii sekarang waktunya reinkarnasi"
+echo "   - Welcome to NOXVER.AI -"
+echo "       - comming soon -"
+axprop $path_axeronprop name -s "NVEX"
+name="NVEX"
+axprop $path_axeronprop id -s "vexa"
+id="vexa
