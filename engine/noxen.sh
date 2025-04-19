ab67447b428b5c7edbf33fc060f9f927af770a75
Reiii3 <mohtoyib79@gmail.com>
2025-04-19 22:37:49 +0700

    update engine/noxen.sh

--- engine/noxen.sh
+++ engine/noxen.sh
@@ -1,1 +1,21 @@
 export devmode=true
+export time=$(date "+%a %b %d %Y %H:%M %Z")
+
+printer() {
+text="$1"
+ i=0
+   while [ $i -lt ${#text} ]; do
+     echo -en "${text:$i:1}"
+ sleep 0.02
+   i=$((i + 1))
+ done
+echo
+}
+
+fle_ins() {
+    folder_path="/data/local/tmp/axeron_cash/zcek_ins"
+    file_name="$1"    # Nama fil
+    # Buat file di dalamnya
+    file_path="$folder_path/$file_name"
+    echo "" > "$file_path"
+}
