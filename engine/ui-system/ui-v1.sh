fbdd454187a059d90f07d90c285f8c5bbe20db97
Reiii3 <mohtoyib79@gmail.com>
2025-04-25 15:25:29 +0700

    update engine/ui-system/ui-v1.sh

--- engine/ui-system/ui-v1.sh
+++ engine/ui-system/ui-v1.sh
@@ -1,12 +1,6 @@
 $AXFUN
-res="/data/local/tmp/axeron_cash/update/noxUp"
 engine="/data/local/tmp/nxver/engine"
-engine_system="/data/local/tmp/engine_core"
 source $engine
-if [[ -f $engine_system ]]; then
-   source $engine_system
-fi
-source $res
 echo "Test ui v1 debug engine"
 printer "selamat datang di ui v1"
 echo
@@ -14,10 +8,3 @@ echo "=============================="
 echo "   TEST UI && PERF FUNCTION"
 echo "=============================="
 echo
-if [[ -z "$perfIns" ]]; then
-   axprop $res perfIns -s "$time"
-   rm "$engine_system"
-   echo "DEBUG : Test setprop terinstall"
-else
-   echo "DEBUG : Setprop sudah terinstall"
-fi
