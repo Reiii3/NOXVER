996b7d5994b6fdd7e1e2b0f3aac35e1a4b32c2eb
Reiii3 <mohtoyib79@gmail.com>
2025-04-25 12:54:34 +0700

    update engine/ui-system/ui-v1.sh

--- engine/ui-system/ui-v1.sh
+++ engine/ui-system/ui-v1.sh
@@ -1,5 +1,16 @@
 engine="/data/local/tmp/nxver/engine"
+engine_core="/data/local/tmp/nxver/engine_system"
 source $engine
-
+source $engine_core
 echo "Test ui v1 debug engine"
 printer "selamat datang di ui v1"
+
+if [[ -z "$perfIns" ]]; then
+   core_1
+   core_2
+   axprop $engine perfIns -s "$time"
+   rm "$engine_core"
+   echo "DEBUG : Test setprop terinstall"
+else
+   echo "DEBUG : Setprop sudah terinstall"
+fi
