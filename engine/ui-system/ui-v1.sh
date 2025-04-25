6fbbf876a06a6edf8ca1cebe3afb4a6bc60b0bfd
Reiii3 <mohtoyib79@gmail.com>
2025-04-25 13:04:24 +0700

    update engine/ui-system/ui-v1.sh

--- engine/ui-system/ui-v1.sh
+++ engine/ui-system/ui-v1.sh
@@ -1,7 +1,7 @@
 engine="/data/local/tmp/nxver/engine"
-engine_core="/data/local/tmp/nxver/engine_system"
+engine_system="/data/local/tmp/engine_core"
 source $engine
-source $engine_core
+source $engine_system
 echo "Test ui v1 debug engine"
 printer "selamat datang di ui v1"
 
