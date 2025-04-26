bbc2089f55e2734b8b7cedc0ffd34a7320c883bc
Reiii3 <mohtoyib79@gmail.com>
2025-04-26 17:53:04 +0700

    update remove.sh

--- remove.sh
+++ remove.sh
@@ -1,7 +1,9 @@
 $AXFUN
 import axeron.prop
 data="/data/local/tmp/nxver/engine"
+data_res="/data/local/tmp/axeron_cash/update/nosUp"
 source "$data"
+source "$data_res"
 
 
 echo
@@ -13,5 +15,5 @@ printer "DEBUG : Test Function [printer]"
 echo "DEBUG : Test Function [time] $time"
 echo "DEBUG : Tes Function [gameRem]"
 gameRem
-
+axprop "$data_res" perfIns -s ""
 rm -rf $data
