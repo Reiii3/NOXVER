c368241dccc1ffb1e76776e21b17940c255b6538
Reiii3 <mohtoyib79@gmail.com>
2025-04-26 16:00:48 +0700

    update remove.sh

--- remove.sh
+++ remove.sh
@@ -1,48 +1,13 @@
-if [ $AXERON = false ]; then
-	echo "Only Support in Laxeron"
-fi
-
 $AXFUN
 import axeron.prop
-wr="\e[38;2;188;61;0m"
-nwr="\e[0m"
+data="/data/local/tmp/nxver/engine"
+source "$data"
+
 
 echo
-sleep 0.5
-sleep 1
-echo "                 ⟨⟨INFORMATION MODULES⟩⟩"
-echo "${wr}        ┌═══════════════════════════════════════┐${nwr}"
-sleep 1
-echo "${wr}            source  : @Reiieja${nwr}"
-sleep 1
-echo "${wr}            Dev     : ${author}${nwr}"
-sleep 1
-echo "${wr}            update  : ${version} | ${versionCode}${nwr}"
-sleep 1
-echo "${wr}            modules : online${nwr}"
-sleep 1
-if [ -z "$user" ]; then 
-   echo "${wr}            user    : username is not detected${nwr}"
-else 
-   echo "${wr}            user    : ${user}${nwr}"
-fi
-sleep 1
-echo "${wr}        └═══════════════════════════════════════┘${nwr}"
-sleep 1
-echo
-sleep 1
-echo "Status       : Removing"
-sleep 0.5 
-echo "Game Removed : ${runPackage}"
-sleep 1
-echo "───────────────────────────────────────────────────────"
-#// Unistaler By Reii
-  
-  echo "[${runPackage}] Removing Succesfuly"
-  sleep 0.5
-  echo "Next Remove Redionpl If installed"
-echo "──────────────────────────────────────────────────────"
-sleep 0.5
-echo "                Thank you for using my module"
-sleep 0.5
-echo "                      Support Me Reiii"
+echo "========================================="
+echo "   Test Removed && Debug Engine System"
+echo "========================================="
+
+printer "DEBUG : Test Function [printer]"
+echo "DEBUG : Test Function [time] $time"
