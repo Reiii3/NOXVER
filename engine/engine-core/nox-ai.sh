1f3918b01783016422df00442d863c778b5882ac
Reiii3 <mohtoyib79@gmail.com>
2025-04-30 15:21:16 +0700

    update engine/engine-core/nox-ai.sh

--- engine/engine-core/nox-ai.sh
+++ engine/engine-core/nox-ai.sh
@@ -3,7 +3,7 @@ bin="/data/local/tmp/nxver"
 engine="$bin/engine"
 gamerun=""
 notif_run=""
-file_update="/Android/data/com.fhrz.axeron/files/noxUp"
+file_update="//data/local/tmp/nxver/.noxUp"
 
 source "$file_update"
 source "$engine"
