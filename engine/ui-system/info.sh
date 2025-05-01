e98fc365fd4c7181954ef5c49ebb1c1b74f9e916
Reiii3 <mohtoyib79@gmail.com>
2025-05-01 22:42:26 +0700

    update engine/ui-system/info.sh

--- engine/ui-system/info.sh
+++ engine/ui-system/info.sh
@@ -1,1 +1,22 @@
+$AXFUN
+data="/data/local/tmp/nxver"
+source_module="$data/.noxUp"
+engine_module="$data/engine"
 
+source $source_module
+source $engine_module
+
+echo
+echo "┌════════════════════════════════┐"
+echo "   information modules && new update"
+echo "└════════════════════════════════┘"
+printer "                   ↓↓"
+echo "==========================================="
+echo -n "   version       : "
+printer "$ver"
+echo -n "   versionCode   : "
+printer "$verc"
+echo -n "   versionEngine : "
+printer "$nameEngine"
+echo "==========================================="
+echo
