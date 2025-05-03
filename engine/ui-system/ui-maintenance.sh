609213f2b3c5fe736858aa091485027e5aa0f958
Reiii3 <mohtoyib79@gmail.com>
2025-05-03 10:50:28 +0700

    update engine/ui-system/ui-maintenance.sh

--- engine/ui-system/ui-maintenance.sh
+++ engine/ui-system/ui-maintenance.sh
@@ -1,10 +1,19 @@
 engine="/data/local/tmp/nxver/engine"
 . $engine
 
-echo
-echo "               - Welcome to NOXVER.AI -"
-echo "                   - comming soon -"
-echo
-echo "changelogs : - AI system fixed"
-echo "             - Test maintenance server"
-echo "             - Add new properties"
+ui_maintenance() {
+   echo
+   echo "               - Welcome to NOXVER.AI -"
+   echo "                   - comming soon -"
+   echo
+   echo "changelogs : - AI system fixed"
+   echo "             - Test maintenance server"
+   echo "             - Add new properties"
+   echo
+}
+
+main() {
+   ui_maintenance
+}
+
+main "$@"
