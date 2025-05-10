3266ff2f8ff890e033c78d6780a67cbdef484bec
Reiii3 <mohtoyib79@gmail.com>
2025-05-10 15:29:14 +0700

    update engine/engine-core/nox-ai.sh

--- engine/engine-core/nox-ai.sh
+++ engine/engine-core/nox-ai.sh
@@ -98,6 +98,9 @@ check_game() {
                echo "DEBUG : Notif Performance Active"
                notif_run="run"
            fi
+           if [[ "$notif_update" == true ]]; then
+              cmd notification post -S bigtext -t "ANNOUNCEMENT" "nox_ai_status" \ "MAINTENANCE SYSTEM"
+           fi
            ai_start
              echo "DEBUG : Performance By Noxver Active"
             if [ "$sperfor" = true ]; then
@@ -121,6 +124,9 @@ check_game() {
                echo "DEBUG : Notif Performance Deactive"
                notif_run="stop"
                ai_end
+            fi
+            if [[ "$notif_update" == true ]]; then
+               cmd notification post -S bigtext -t "ANNOUNCEMENT" "nox_ai_status" \ "MAINTENANCE SYSTEM"
             fi
             echo "DEBUG : Performance By Noxver Deactivated"
             if [ "$sperfor" = true ]; then
