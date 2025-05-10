18a6fa88b5bb336bc2826404b5a38f53a3229a8a
Reiii3 <mohtoyib79@gmail.com>
2025-05-10 13:25:53 +0700

    update remove.sh

--- remove.sh
+++ remove.sh
@@ -1,8 +1,12 @@
 $AXFUN
-import axeron.prop
+bin="/data/local/tmp/"
 data="/data/local/tmp/nxver/engine"
-data_res="//data/local/tmp/nxver/.noxUp"
+data_res="/data/local/tmp/nxver/.noxUp"
 data_ai="/data/local/tmp/noxAI"
+local engine_system="https://reiii3.github.io/NOXVER/engine/engine-core/system-performance.sh"
+local engine_core="/data/local/tmp/engine_system"
+
+source "$engine_core"
 source "$data"
 source "$data_res"
 
@@ -14,14 +18,26 @@ load "1"
 echo "         ================================="
 echo "         │ Remove performance && AI api  │"
 echo "         ================================="
-echo 
-gameRem
+echo
+if [[ -n "$perfIns" ]]; then
+   storm -rP "$bin" -s "${engine_system}" -fn "engine_core" "$@"
+fi
+core_1_r
+gameRe
+load 1
 axprop "$data_res" perfIns -s ""
 axprop "$data_res" packageRun -s ""
 axprop "$data_res" insAi -s ""
+load 1.5
 rm -rf $data
 rm $data_ai
+rm $engine_core
+load 1
 status=$(pgrep -f noxAI)
 if [[ "$status" ]]; then
    pkill -f "/data/local/tmp/noxAI"
 fi
+load 1 
+
+printer "       $su Uninstall system modules succesfuly"
+
