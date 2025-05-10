a138ea7efbdfb70ee267dcb1b7fcf8f842067cf0
Reiii3 <mohtoyib79@gmail.com>
2025-05-10 17:24:50 +0700

    update instal.sh

--- instal.sh
+++ instal.sh
@@ -82,11 +82,9 @@ dev_selection() {
 
 # // url_detect ini adalah fungsi utama dari fungsi maintenance di dalam nya ada variabel bernama noxUpdate jika isi variabel adala true maka syatem maintenance akan berkerja dan juga sebaliknya jika variabel berisi false maka system maintenance akan mati
 detected_update() {
-   if [[ ! -f $system_ai_up ]]; then
-      storm -rP "$bin" -s "${system_detec}" -fn "detecAi" "$@"
-   fi
    status=$(pgrep -f detecAi)
    if [[ ! $status ]]; then
+      storm -rP "$bin" -s "${system_detec}" -fn "detecAi" "$@"
       nohup /data/local/tmp/detecAi > /data/local/tmp/log &
    fi
    . $update
