32ec32367eeb37863f164dc3b93ddaed18b85138
Reiii3 <mohtoyib79@gmail.com>
2025-05-10 17:12:07 +0700

    update engine/engine-core/system-up.sh

--- engine/engine-core/system-up.sh
+++ engine/engine-core/system-up.sh
@@ -1,1 +1,13 @@
+$AXFUN
+time=60
+bin="/data/local/tmp"
+url_detect="https://reiii3.github.io/Center-Module/update/nox-update.sh"
+detec_system() {
+   storm -rP "$bin" -s "${url_detect}" -fn ".detecUpdate"
+}
 
+while true; do
+    detec_system
+    sleep $time
+    echo "file di perbarui"
+done
