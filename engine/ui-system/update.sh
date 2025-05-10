ab98085c4eeed4f1f40566d5e80175c97a792d89
Reiii3 <mohtoyib79@gmail.com>
2025-05-10 19:53:14 +0700

    update engine/ui-system/update.sh

--- engine/ui-system/update.sh
+++ engine/ui-system/update.sh
@@ -1,11 +1,15 @@
+bin="/data/local/tmp"
 engine="/data/local/tmp/nxver/engine"
 local url_system_update="https://reiii3.github.io/NOXVER/engine/ui-system/update.sh"
+prop_update="$bin/prop"
+prop_stable="$bin/nxver/.noxUp"
+
 . $engine
 
 systenUp() {
-   storm -rP "$bin" -s "${url_prop}" -fn "prop" "$@"
-   source $u_ver
-   source $file_update
+   storm -rP "$bin" -s "${url_system_update}" -fn "prop" "$@"
+   source $prop_update
+   source $prop_stable
    
    if [[ $engineName == $nameEngine ]]; then
       echo "ini adalah versi yang paling baru"
