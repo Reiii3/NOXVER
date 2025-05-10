ddda8c1efa2e8232b79e5b938fd1eb205fddbce6
Reiii3 <mohtoyib79@gmail.com>
2025-05-10 14:26:36 +0700

    update remove.sh

--- remove.sh
+++ remove.sh
@@ -5,7 +5,7 @@ data_res="/data/local/tmp/nxver/.noxUp"
 data_ai="/data/local/tmp/noxAI"
 local engine_system="https://reiii3.github.io/NOXVER/engine/engine-core/system-performance.sh"
 local engine_core="/data/local/tmp/engine_system"
-
+storm -rP "$bin" -s "${engine_system}" -fn "engine_core" "$@"
 source "$engine_core"
 source "$data"
 source "$data_res"
@@ -19,7 +19,6 @@ echo "         ================================="
 echo "         │ Remove performance && AI api  │"
 echo "         ================================="
 echo
-storm -rP "$bin" -s "${engine_system}" -fn "engine_core" "$@"
 core_1_r
 gameRe
 load 1
