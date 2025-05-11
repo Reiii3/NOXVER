2e6fa564d269a01ff33b1fafc10262abead39c53
Reiii3 <mohtoyib79@gmail.com>
2025-05-11 19:24:44 +0700

    update engine/ui-system/ui-v1.sh

--- engine/ui-system/ui-v1.sh
+++ engine/ui-system/ui-v1.sh
@@ -4,9 +4,9 @@ source $engine
 
 #---------------- main ui modules-----------------
 echo
-printer "                   - NOXVER.AI -"
+printer "                     - NOXVER.AI -"
 load "1"
-echo "      ======================================"
-echo "      │ Instalation performance && AI api  │"
-echo "      ======================================"
+echo "        ======================================"
+echo "        │ Instalation performance && AI api  │"
+echo "        ======================================"
 echo ""
