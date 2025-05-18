5c941029b09860a6edb289e7415d9e5f68106d98
Reiii3 <mohtoyib79@gmail.com>
2025-05-18 18:18:00 +0700

    update engine/ui-system/change.sh

--- engine/ui-system/change.sh
+++ engine/ui-system/change.sh
@@ -1,1 +1,29 @@
+bin="/data/local/tmp"
+engine="$bin/nxver/engine"
+prop="$bin/nxver/.noxUp"
 
+source $engine
+source $prop
+
+brand_noxver ice
+echo "====================================="
+echo "   Welcome To Changelogs NOXVER.AI"
+echo "====================================="
+echo 
+echo " Usage This Module :"
+echo "    Instalation this Module : ax vex "
+echo "    Removed this Module : ax vex -r"
+echo "    Module this Information : ax vex -i | ax vex --info"
+echo "    Module this Changelogs : ax vex -c | ax vex --changelogs"
+echo "    Update this module [ If Any ] : ax vex -update"
+echo "    Rebooot System thus Module : ax vex -aR"
+echo
+echo " Information New Update :"
+echo "    - Optimalisasi System AI Noxver"
+echo "    - Remaking the AI notification design"
+echo "    - Added Game Driver Option"
+echo "    - Stabilizing Game Compiler"
+echo "    - Stabilizing the skiavk renderer"
+echo
+echo " plugin NOXVER :"
+echo "    - comming soon"
