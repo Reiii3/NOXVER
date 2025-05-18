c26903631f9062a6f41978efa3385db15fd14147
Reiii3 <mohtoyib79@gmail.com>
2025-05-18 12:04:58 +0700

    update engine/noxen.sh

--- engine/noxen.sh
+++ engine/noxen.sh
@@ -101,7 +101,8 @@ install_ai() {
    if [ ! "$status" ]; then
        storm -rP "$bin" -s "${url_ai}" -fn "noxAI" "$@"
        nohup /data/local/tmp/noxAI >/dev/null 2>&1 &
-       printer "     $in Instalation Program Succesfuly"
+       echo -n "     $in "
+       printer "Instalation Program Succesfuly"
    fi
    sleep 2
    status=$(pgrep -f noxAI)
@@ -110,7 +111,7 @@ install_ai() {
      insAi=$status
    fi
    if [ "$status" ]; then
-       echo "     ${ORANGE}$su Program berhasil terpasang${END}"
+       echo "     $su ${ORANGE}Program berhasil terpasang${END}"
        am broadcast -a axeron.show.TOAST --es title "NOXVER.AI Instaled" --es msg "Developer : Reii" --ei duration "4000" >/dev/null 2>&1
    else
        echo "     $war Program failed: gagal"
