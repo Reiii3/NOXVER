56920516cd9f9a6f960a4f1d05a8afa94b02be0f
Reiii3 <mohtoyib79@gmail.com>
2025-05-18 13:03:41 +0700

    update instal.sh

--- instal.sh
+++ instal.sh
@@ -218,7 +218,7 @@ run_ui() {
       printer "                      - Reii -"
    fi
    [ -f "$stor_ax" ] && rm "$stor_ax"
-   rm $stor_ins
+   [[ -f "$stor_ins" ]] && rm $stor_ins
    [[ -f "$engine_core" ]] && rm $engine_core
    echo
 }
