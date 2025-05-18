0d12b0a2a14d4663a8354eb4f3c641c19c2cde85
Reiii3 <mohtoyib79@gmail.com>
2025-05-18 23:39:30 +0700

    update engine/ui-system/change.sh

--- engine/ui-system/change.sh
+++ engine/ui-system/change.sh
@@ -12,11 +12,21 @@ echo "         ====================================="
 echo 
 echo " Usage This Module :" && load 0.5
 echo "    - Instalation this Module : ax vex " && load 0.5
+echo "       • fungsi ini di gunakan untuk instalasi"
+echo "         module"
 echo "    - Removed this Module : ax vex -r" && load 0.5
+echo "       • fungsi ini di gunakan untuk remove module"
 echo "    - Information this Module : ax vex -i" && load 0.5
+echo "       • ini berisi tentang semua informasi tentang"
+echo "         module ini seperti versi,dev,dll"
 echo "    - Changelogs this Module : ax vex -c " && load 0.5
+echo "       • ini adalah semua penjelasan tentang fitur"
+echo "         di module ini"
 echo "    - Update this module [ If Any ] : ax vex -update" && load 0.5
+echo "       • Ini di lakukan saat ada anouncement update"
 echo "    - Rebooot System this Module : ax vex -aR" && load 0.5
+echo "       • Biasanya ini di gunakan ketika anda baru"
+echo "         download game baru"
 echo
 echo " Information New Update :" && load 0.5
 echo "    - Optimalisasi System AI Noxver" && load 0.5
@@ -30,3 +40,9 @@ echo "    - Comming soon" && load 0.5
 echo "    - Comming soon" && load 0.5
 echo "    - Comming soon" && load 0.5
 echo 
+echo " NOTES :"
+echo "  Jika kalian akan melakukan update usahakan melakukan"
+echo "  Remove terlebih dahulu pada module kalian biar tidak"
+echo "  terjadi eror pada versi terbaru nya, terima kasih.."
+echo
+echo "                ©® Copyright By ReiiEja"
