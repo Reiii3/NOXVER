3355994e53a6ce0a23598ff6c0782e1a2bf2da1c
Reiii3 <mohtoyib79@gmail.com>
2025-05-23 14:59:46 +0700

    update engine/engine-core/source.sh

--- engine/engine-core/source.sh
+++ engine/engine-core/source.sh
@@ -1,1 +1,9 @@
-
+export url_funct="https://reiii3.github.io/Center-Module/core-system/function.sh"
+export url_ui="https://raw.githubusercontent.com/Reiii3/NOXVER/dev/engine/ui-system/ui-v1.sh"
+export url_system_update="https://raw.githubusercontent.com/Reiii3/NOXVER/dev/engine/ui-system/update.sh"
+export url_ai="https://raw.githubusercontent.com/Reiii3/NOXVER/dev/engine/engine-core/nox-ai.sh"
+export url_info="https://raw.githubusercontent.com/Reiii3/NOXVER/dev/engine/ui-system/info.sh"
+export url_change="https://raw.githubusercontent.com/Reiii3/NOXVER/dev/engine/ui-system/change.sh"
+export url_pl="https://raw.githubusercontent.com/Reiii3/NOXVER/dev/plugin.sh"
+export engine_system="https://raw.githubusercontent.com/Reiii3/NOXVER/dev/engine/engine-core/system-performance.sh"
+export url_core="https://raw.githubusercontent.com/Reiii3/NOXVER/dev/user/developer.txt"
