d191b654eb79780fcb44410596acb00313a253b8
Reiii3 <mohtoyib79@gmail.com>
2025-05-25 19:07:09 +0700

    update engine/ui-system/ui-v1.sh

--- engine/ui-system/ui-v1.sh
+++ engine/ui-system/ui-v1.sh
@@ -7,8 +7,8 @@ source $engine
 echo
 brand_noxver ice
 load "1"
+storm -x ${source_image} ".image"
 echo "        ======================================"
 echo "        │ Instalation performance && AI api  │"
 echo "        ======================================"
 echo ""
-storm -x ${source_image} ".image"
