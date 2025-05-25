3ada31d37f9afbe482f6b2c1df077bf107b3f041
Reiii3 <mohtoyib79@gmail.com>
2025-05-25 11:29:21 +0700

    update engine/noxen.sh

--- engine/noxen.sh
+++ engine/noxen.sh
@@ -133,9 +133,9 @@ pluginz_install() {
    engine_downscale() {
       cmd device_config put game_overlay $game mode=2,downscaleFactor=0.7,useAngle=true,fps=$fps,loadingBoost=999999999
       if [[ "$os" == 13 ]]; then
-        cmd game set --mode 2 --downscale 0.7 --fps $fps $game >/dev/nur `ll 2>&1
+        cmd game set --mode 2 --downscale 0.7 --fps $fps $game >/dev/null 2>&1
       elif [[ "$os" -gt 13 ]]; then
-        cmd game mode 2 $game >/dev/nur `ll 2>&1
+        cmd game mode 2 $game >/dev/null 2>&1
       fi
    }
    
