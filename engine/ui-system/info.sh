95365f0e85c605cd06740c3b9130c0823c92d7d7
Reiii3 <mohtoyib79@gmail.com>
2025-05-25 13:14:02 +0700

    update engine/ui-system/info.sh

--- engine/ui-system/info.sh
+++ engine/ui-system/info.sh
@@ -14,22 +14,22 @@ source $source_module
 source $engine_module
 
 echo -n "┌$pr " && printer "Information NOXVER.AI - v1.0"
-echo -n "├$pr " && printer "version : ${ver:-null}"
-echo -n "├$pr " && printer "version-code : $verc"
+echo -n "├$pr " && printer "version        : ${ver:-null}"
+echo -n "├$pr " && printer "version-code   : $verc"
 echo -n "├$pr " && printer "version-engine : $nameEngine"
-echo -n "├$pr " && printer "developer : @ReiiEja - Telegram"
-echo -n "└┬$pr " && printer "build-date : $buildDate"
+echo -n "├$pr " && printer "developer      : @ReiiEja - Telegram"
+echo -n "└┬$pr " && printer "build-date   : $buildDate"
 echo -n " ├$pr " && printer "last install : ${perfIns:-null}"
-echo -n " ├$pr " && printer "last update : ${waktuUpn-null}"
+echo -n " ├$pr " && printer "last update  : ${waktuUpn-null}"
 echo -n " ├$pr " && printer "plugin install : ${plugins:-null}"
 if pgrep -f noxAI >/dev/null 2>&1; then
-   echo -n "┌┴$pr " && printer "status AI : actived"
+   echo -n "┌┴$pr " && printer "status AI      : actived"
 else
-   echo -n "┌┴$pr " && printer "status AI : deactived"
+   echo -n "┌┴$pr " && printer "status AI      : deactived"
 fi
 echo -n "├$pr " && printer "Information Your Phone [•]"
 echo -n "├$pr " && printer "android-version : $os"
-echo -n "├$pr " && printer "chipset : $chip"
-echo -n "├$pr " && printer "gpu : $gpu"
-echo -n "└$pr " && printer "Brand : $model"
+echo -n "├$pr " && printer "chipset         : $chip"
+echo -n "├$pr " && printer "gpu             : $gpu"
+echo -n "└$pr " && printer "Brand           : $model"
 echo
